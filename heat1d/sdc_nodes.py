18ab1b73e31af07f1333f5afc6c0ab2af10e539b
Fabian Köhler <fkoehler1024@googlemail.com>
2015-08-18 09:49:26 +0200

    remove torbjörn's pfasst

--- heat1d/sdc_nodes.py
+++ heat1d/sdc_nodes.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import heat1d
 
+plt.xscale("log")
 plt.yscale("log")
 
 runner = heat1d.Heat1D_SDC()
@@ -11,6 +12,10 @@ def case(nodes):
     residuals = runner.absolute_residuals(runner.run())
     iters     = np.arange(1, len(residuals)+1)
     plt.plot(iters, residuals, ".-", label=str(nodes)+" Nodes")
+
+def theory(order):
+    x = np.linspace(1, 18, 100)
+    plt.plot(x, x**(-order), "--")
 
 plt.title(
     r"Heat1D with SDC\\"+
@@ -25,8 +30,13 @@ nodes = [3,5,7]
 for n in nodes:
     case(n)
 
+# orders = [2,4,6,8]
+# for o in orders:
+#     theory(o)
+
 plt.xlabel("iteration")
 plt.ylabel("absolute residuals")
 
 plt.legend()
+plt.show()
 plt.savefig("plot/heat1d/sdc_nodes.pdf")
